2ef03e19c94af836f89081e4fa756c89fdf5d682
Team 9 <team9@stuba.sk>
2017-05-22 23:15:50 +0200

    [EB] pridane php load navbaru a headeru

--- general_functions.php
+++ general_functions.php
@@ -84,3 +84,83 @@ function loginLDAP($login, $password){
     @ldap_close($connect);
     return(false);
 }
+function loadHead(){
+    echo    '<meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <link rel="stylesheet" href="css/eb_general.css">
+                <link href="css/bootstrap.min.css" rel="stylesheet">
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+                <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">';
+}
+
+function loadNavbar(){
+    echo '    <nav class="navbar navbar-default navbar-fixed-top" id="navbar-custom">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#emNavbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand navbar-brand-logo" href="#">
+                    <div class="logo">
+                        <img id="logoIMG" src="./images/logo/logo_skratkove_transparentne_na_modre_pozadie.png" width="167" alt="logo">
+                    </div>
+                </a>
+            </div>
+            <div class="collapse navbar-collapse" id="emNavbar">
+                <ul class="nav navbar-nav navbar-right scrollable-menu">
+                    <li><a href="#" class="navbarItem">O nás</a></li>
+                    <li><a href="#" class="navbarItem">Pracovníci</a></li>
+                    <li><a href="#" class="navbarItem">Štúdium</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle navbarItem" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Výskum <b class="caret"></b></a>
+                        <ul class="dropdown-menu multi-level">
+                            <li><a href="#" class="navbarItem">Projekty</a></li>
+                            <li class="dropdown-submenu dropdown">
+                                <a href="#" class="dropdown-toggle navbarItem" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Výskumné oblasti <b class="caret"></b></a>
+                                <ul class="dropdown-menu submenuItem" >
+                                    <li><a href="#" >Elektrická motokára</a></li>
+                                    <li><a href="#" >Autonómne vozidlo 6×6</a></li>
+                                    <li><a href="#" >3D LED kocka</a></li>
+                                    <li><a href="#" >Biomechatronika</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <li><a href="#" class="navbarItem">Aktuality</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle navbarItem" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Aktivity <b class="caret"></b></a>
+                        <ul class="dropdown-menu multi-level">
+                            <li><a href="#" class="navbarItem">Fotogaléria</a></li>
+                            <li><a href="#" class="navbarItem">Videá</a></li>
+                            <li><a href="#" class="navbarItem">Média</a></li>
+                            <li class="dropdown-submenu">
+                                <a href="#" class="dropdown-toggle navbarItem" data-toggle="dropdown">Naše témetické web stránky <b class="caret"></b></a>
+                                <ul class="dropdown-menu submenuItem2 navbarItem" >
+                                    <li><a href="http://www.e-mobilita.fei.stuba.sk/" >Elektromobilita</a></li>
+                                </ul>
+                            </li>
+
+                        </ul>
+                    </li>
+                    <li><a href="#" class="navbarItem">Kontakt</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle navbarItem" data-toggle="dropdown"><span class="glyphicon glyphicon-globe"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#" class="navbarItem"><span class="glyphicon glyphicon-flag"></span>  SK</a></li>
+                            <li><a href="#" class="navbarItem"><span class="glyphicon glyphicon-flag"></span>  EN</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#" class="navbarItem"><span class="glyphicon glyphicon-user"></span></a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>';
+}
+
+function loadJScripts(){
+    echo '<script src="js/jquery.js"></script>
+        <script src="js/bootstrap.min.js"></script>';
+}
