92e8218654e5fc7d2162047b2f83a9f00bfc4cc0
Team 9 <team9@stuba.sk>
2017-05-25 18:02:49 +0200

    [SS] index-fb

--- sk/index.php
+++ sk/index.php
@@ -22,12 +22,20 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
-                    <h1>Nadpis</h1>
-                    <hr>
-                        <div>
-                            <p>Tu mozete smelo davat co chcete</p>
-
-                        </div>
+                            <div id="fb-root"></div>
+                                <script>(function(d, s, id) {
+                                  var js, fjs = d.getElementsByTagName(s)[0];
+                                  if (d.getElementById(id)) return;
+                                  js = d.createElement(s); js.id = id;
+                                  js.src = "//connect.facebook.net/sk_SK/sdk.js#xfbml=1&version=v2.9";
+                                  fjs.parentNode.insertBefore(js, fjs);
+                                }(document, 'script', 'facebook-jssdk'));
+                               </script>
+                            <div class="fb-page" data-href="https://www.facebook.com/UAMTFEISTU/?fref=ts" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false">
+                                <blockquote cite="https://www.facebook.com/UAMTFEISTU/?fref=ts" class="fb-xfbml-parse-ignore">
+                                    <a href="https://www.facebook.com/UAMTFEISTU/?fref=ts">Ústav automobilovej mechatroniky FEI STU</a>
+                                </blockquote>
+                            </div>
                 </div>
             </div>
         </div>
