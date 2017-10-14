086ee71843e284573d8eceb3de039bd23214def0
UAMT - FEI <uamt@stu.sk>
2017-10-14 11:32:19 +0200

    prepar for work

--- config.php
+++ config.php
@@ -5,20 +5,35 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 $hostname = "localhost";
-$username = "tim9";
-$password = "tim9";
-$dbname = "final";
+$username = "tp1718a";
+$password = "Uamt2017";
+$dbname = "tp1718a";
 
 define("HOSTNAME", "localhost");
-define("USERNAME", "tim9");
-define("PASSWORD", "tim9");
-define("DBNAME", "final");
+define("USERNAME", "tp1718a");
+define("PASSWORD", "Uamt2017");
+define("DBNAME", "tp1718a");
 
 $dbconfig = array(
     'hostname' => 'localhost',
-    'username' => 'tim9',
-    'password' => 'tim9',
-    'dbname' => 'final',
+    'username' => 'tp1718a',
+    'password' => 'Uamt2017',
+    'dbname' => 'tp1718a',
 ); 
+
+function new_connection() {
+	$hostname = "localhost";
+	$username = "tp1718a";
+	$password = "Uamt2017";
+	$dbname = "tp1718a";
+	$conn = new mysqli($hostname, $username, $password, $dbname);
+
+	if ($conn->connect_error) {
+		  die("Connection failed: " . $conn->connect_error);
+	}
+	mysqli_set_charset($conn, "utf8");
+
+	return $conn;
+}
 
 ?>
--- general_functions.php
+++ general_functions.php
@@ -2,22 +2,6 @@
 
 require_once __DIR__ . '/config.php';
 
-function new_connection() {
-        $hostname = "localhost";
-        $username = "tim9";
-        $password = "tim9";
-        $dbname = "final";
-
-        $conn = new mysqli($hostname, $username, $password, $dbname);
-
-        if ($conn->connect_error) {
-              die("Connection failed: " . $conn->connect_error);
-        }
-        mysqli_set_charset($conn, "utf8");
-
-        return $conn;
-}
-
 function loginLDAP($login, $password){
     $ldap_server = "ldap.stuba.sk";
 
