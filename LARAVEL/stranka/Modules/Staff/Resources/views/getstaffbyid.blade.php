c55cc66e70a3608504c755fa32b7636359bd520c
xsimcikova <simona.simcikova@gmail.com>
2017-11-16 23:35:50 +0100

    edited staff-detail page

--- LARAVEL/stranka/Modules/Staff/Resources/views/getstaffbyid.blade.php
+++ LARAVEL/stranka/Modules/Staff/Resources/views/getstaffbyid.blade.php
@@ -16,8 +16,46 @@ $(document).ready(function() {
 
 
 </script>
-<div id="emPAGEcontent">
-<div class="container">
+<section class="banner banner--big" style="background-image: url('{{ URL::asset('images/banners/banner2.jpg') }}')">
+
+</section>
+<section class="staff-profile">
+	<div class="container">
+		<div class="staff-profile__content">
+			<div class="staff-profile__info">
+				<div class="row">
+					<div class="col-sm-4 staff-profile__img">
+						<img src="{{ URL::asset('images/staffPhoto') }}/{{ $ais->photo }}" alt="{{ $ais->photo }}">
+					</div>
+					<div class="col-sm-offset-1 col-sm-7">
+						<a href="{{ url('/staff') }}" class="staff-profile__back"><i class="fa fa-arrow-left"></i>Späť na všetkých zamestnancov</a>
+						<h2>{{ $ais->title1 }} {{ $ais->name }} <b>{{ $ais->surname }}</b>, {{ $ais->title2 }}</h2>
+						<span class="staff-profile__role">{{ $ais->staffRole }}</span>
+						<hr>
+						<div class="staff-profile__description">
+							<p><span>Kancelária: </span>{{ $ais->room }}</p>
+							<p><span>Klapka: </span>{{ $ais->phone }}</p>
+							<p><span>Oddelenie: </span>{{ $ais->department }}</p>
+							<p><span>Funkcia: </span>{{ $ais->function }}</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="staff-profile__contact">
+				<div class="staff-profile__links">
+					<a href=""><i class="fa fa-envelope" aria-hidden="true"></i></a>
+					<a href=""><i class="fa fa-paper-plane" aria-hidden="true"></i></a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="staff-publications">
+	<div class="staff-publications__button">
+		<button>Zobraziť publikácie</button>
+	</div>
+</section>
+<!-- <div class="container">
 	<div class="row">
 		<div class="col-lg-12">
 		<a href="{{ url('/staff') }}" class="btn btn-default">Späť</a>
@@ -60,6 +98,6 @@ $(document).ready(function() {
 			</div>
 		</div>
 	</div>
-</div>
+</div> -->
 
 @stop
