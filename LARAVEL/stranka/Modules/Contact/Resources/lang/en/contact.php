2cde7349d40f6261759f700df9c280552677c5d2
Matus Dzacovsky <matus753@gmail.com>
2017-12-03 16:22:06 +0100

    refactor contact

---
+++ LARAVEL/stranka/Modules/Contact/Resources/lang/en/contact.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'stu' => 'Slovak University of Technology',
+    'fei' => 'Faculty of Electrical Engineering and Information Technology',
+    'sk' => 'Slovak Republic',
+    'institute' => 'Institute of Automotive Mechatronics',
+    'secretariat' => 'Secretariat',
+    'room' => 'Room',
+    'phone' => 'Phone'
+];
--- LARAVEL/stranka/public/js/additional_js.js
+++ LARAVEL/stranka/public/js/additional_js.js
@@ -11,3 +11,19 @@ function scrollToTop(){
 		scrollTop: $('html').offset().top             
     }, 500);
 }
+
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 14,
+        center: {lat: 48.151834, lng: 17.073337},
+        mapTypeId: 'roadmap'
+    });
+
+    // Construct the circle for each value in citymap.
+    // Note: We scale the area of the circle based on the population.
+    var marker = new google.maps.Marker({
+        position: {lat: 48.151834, lng: 17.073337},
+        map: map,
+        title: 'STU'
+    });
+}
