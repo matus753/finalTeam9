7856398cca3c3018093de18234e072caa53b635a
Matus Dzacovsky <matus753@gmail.com>
2017-12-05 00:24:41 +0100

    fixed untracked fileas

--- LARAVEL/stranka/storage/framework/views/f9da3a3332a1a394d9cabc021eb374d53ceb9e11.php
+++
@@ -1,4 +0,0 @@
-<?php $__env->startSection('menu'); ?>
-	<p>MENUUUU</p>
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('base_structure', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
