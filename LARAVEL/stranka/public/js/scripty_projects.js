605c007a606c617b19a5921441b1a1eaff856c80
emily <milka.brisova@gmail.com>
2018-02-26 22:49:48 +0100

    uprava projektov

--- LARAVEL/stranka/public/js/scripty_projects.js
+++ LARAVEL/stranka/public/js/scripty_projects.js
@@ -1,43 +1,18 @@
 $('.m').on("click",function(){
-    // console.log($(this).data('href'));
-    $.ajax({
-        type: 'POST',
-        url: $(this).data('href'),
-        headers: {
-            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        },
-        success: function(msg){
-            var obj = JSON.parse(msg);
-            if ($('#lang').html() === 'sk') {
-                $("#modalTitle").html(obj.titleSK);
-            } else {
-                $("#modalTitle").html(obj.titleEN);
-            }
-            $("#modalType").html(obj.projectType);
-            $("#modalNumber").html(obj.number);
-            $("#modalDuration").html(obj.duration);
-            $("#modalCoordinator").html(obj.coordinator);
-            if (obj.partners){
-                $( "#modalPartnersDiv" ).removeClass( "defHide" );
-                $("#modalPartners").html(obj.partners);
-            }
-            if (obj.web){
-                $( "#modalWebDiv" ).removeClass( "defHide" );
-                $("#modalWeb").html(obj.web);
-            }
-            if (obj.internalCode){
-                $( "#modalCodeDiv" ).removeClass( "defHide" );
-                $("#modalCode").html(obj.internalCode);
-            }
-            if (obj.annotationSK && $('#lang').html() === 'sk' ){
-                $( "#modalAnotDiv" ).removeClass( "defHide" );
-                $("#modalAnot").html(obj.annotationSK);
-            }
-            if (obj.annotationEN && $('#lang').html() === 'en' ){
-                $( "#modalAnotDiv" ).removeClass( "defHide" );
-                $("#modalAnot").html(obj.annotationEN);
-            }
-
-        }
-    });
+    $isClicked = 'sub' + $(this).data("id");
+    if ($('.'+$isClicked).css("text-transform") == "uppercase"){
+        $('.'+$isClicked).css("text-transform", "none");
+    } else {
+        $('.'+$isClicked).css("text-transform", "uppercase");
+    }
+});
+$('.m').hover(function () {
+    $(this).css("background-color", "rgb(230, 230, 230)");
+}, function () {
+    $isClicked = 'sub' + $(this).data("id");
+    if ($('.'+$isClicked).css("text-transform") == "uppercase"){
+        $(this).css("background-color", "rgb(242, 242, 242)")
+    } else {
+        $(this).css("background-color", "#fff")
+    }
 });
