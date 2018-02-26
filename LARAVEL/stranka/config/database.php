ca09e07db21807d4a223df1280b1e478abdd2456
emiliaBrisova <milka.brisova@gmail.com>
2018-02-26 23:23:54 +0100

    Update database.php
    uprava db config

--- LARAVEL/stranka/config/database.php
+++ LARAVEL/stranka/config/database.php
@@ -39,7 +39,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'heslo123'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
