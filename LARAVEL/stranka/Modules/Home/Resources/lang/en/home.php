04c59b5f9428b1c20eaa01adf64cd3320bdf09d9
biac <biacko.ivan@gmail.com>
2018-03-05 02:08:03 +0100

    [IB] prelozenie tagov, ktore boli vytvorene a neprelozene

--- LARAVEL/stranka/Modules/Home/Resources/lang/en/home.php
+++ LARAVEL/stranka/Modules/Home/Resources/lang/en/home.php
@@ -2,13 +2,13 @@
 
 return [
 
-	'slider1' => 'Welcome to page',
+	'slider1' => 'Welcome to the page',
 	'slider1_highlighted' => 'Institute of Automotive Mechatronics FEI STU',
 	'slider1_link' => '',
 	'slider2' => 'doc. Ing. Peter DRAHOŠ, PhD.',
-	'slider2_highlighted' => '"OUR PRIORITY ARE SUCCESSFUL STUDENTS"',
+	'slider2_highlighted' => '"SUCCESSFUL STUDENTS ARE OUR PRIORITY"',
 	'slider2_link' => '',
-	'slider3' => 'Visit our promotion page',
+	'slider3' => 'Visit our promotional page',
 	'slider3_highlighted' => 'AUTOMOTIVE MECHATRONICS',
 	'slider3_link' => 'HERE',
 	
@@ -17,19 +17,19 @@ return [
 	'staff' => 'Staff',
 	
 	'news' => 'News',
-	'news_section_preview' => 'What is going on and what has ever been done at our institute can be found on the News subpage.',
+	'news_section_preview' => 'What is going on and what has been done so far at our institute can be found on the News subpage.',
 	
 	'photo_gallery' => 'Photos',
-	'photo_gallery_section_preview' => 'Photos and videos from our stock will bring you closer to life on our faculty.',
+	'photo_gallery_section_preview' => 'Photos and videos from our events will tell you a little more about life on our faculty.',
 	'videos' => 'Videos',
 	
 	'offering' => 'WHAT WE OFFER',
 	
-	'dis_master_study' => 'DISTANCT MASTER STUDY',
-	'dis_master_study_text' => 'From the school year 2017/2018, we are opening the distance form of engineering studies in the modern study program Applied Mechatronics and Electromobility.',
+	'dis_master_study' => 'DISTANT MASTER STUDY',
+	'dis_master_study_text' => 'Since year 2017/2018, the distance form of engineering studies is open in the modern study program Applied Mechatronics and Electromobility.',
 	
 	'quality_of_studies' => 'QUALITY OF STUDIES',
-	'quality_of_studies_text' => 'Are you interested in computer science, electronic systems, mechanics and automatic control? All these areas can be studied simultaneously in one study program.',
+	'quality_of_studies_text' => 'Are you interested in computer science, electronic systems, mechanics and automatic control? All of these areas can be studied simultaneously in one study program.',
 	
 	'interesting_projects' => 'WORKING ON INTERESTING PROJECTS',
 	'interesting_projects_text' => 'Our institute cooperates with a number of companies and is involved in several domestic or international projects.',
