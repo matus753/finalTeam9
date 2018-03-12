28c39a5151bad477124800ee0cee4673aec2e3ef
Simona Šimčíková <simona.simcikova@gmail.com>
2018-03-12 20:05:02 +0100

    uprava homepage

--- LARAVEL/stranka/Modules/Home/Resources/views/home.blade.php
+++ LARAVEL/stranka/Modules/Home/Resources/views/home.blade.php
@@ -55,22 +55,11 @@
 	</div>
 </div>
 	
-	<div class="container">
-		<div class="row"> 
-			<div class="col-lg-12">
-				<div class="row thumb-fb-row hp-thumbnail">
-					<div class="col-sm-4 col-md-3">
-					  <div class="hp-thumbnail__wrapper">
-						  <a class="hp-thumbnail__item" style="background-image: url({{ URL::asset('images/indexPhoto/t1.JPG') }})" href="management">
-						  	<div class="hp-thumbnail__item-content">
-						  		<h3>@lang('home::home.about_us')</h3>
-						  		<p class="hp-thumbnail__text">@lang('home::home.about_us_section_preview')</p>
-						  		<p class="hp-thumbnail__link">Prejdite na stránku <i class="fa fa-arrow-right" aria-hidden="true"></i></p>
-						  	</div>
-						  </a>
-					</div>
-				  </div>
-				  <div class="col-sm-4 col-md-3">
+<div class="container">
+	<div class="row hp-row"> 
+		<div class="col-md-6">
+			<div class="row hp-thumbnail">										
+				  <div class="col-sm-6 col-md-6">
 					  <div class="hp-thumbnail__wrapper">
 						  <a class="hp-thumbnail__item" style="background-image: url({{ URL::asset('images/indexPhoto/t2.JPG') }})" href="news">
 						  	<div class="hp-thumbnail__item-content">
@@ -79,9 +68,9 @@
 						  		<p class="hp-thumbnail__link">Prejdite na stránku <i class="fa fa-arrow-right" aria-hidden="true"></i></p>
 						  	</div>
 						  </a>
-					  </div>
+					  </div>				  
 				  </div>
-				  <div class="col-sm-4 col-md-3">
+				  <div class="col-sm-6 col-md-6">
 					  <div class="hp-thumbnail__wrapper">
 						  <a class="hp-thumbnail__item" style="background-image: url({{ URL::asset('images/indexPhoto/t3.JPG') }})" href="photo-gallery">
 						  	<div class="hp-thumbnail__item-content">
@@ -92,92 +81,96 @@
 						  </a>
 					  </div>
 				  </div>
-				  <div class="col-sm-12 col-md-3">                          
-						<div id="fb-root"></div>
-							<script>(function(d, s, id) {
-							  var js, fjs = d.getElementsByTagName(s)[0];
-							  if (d.getElementById(id)) return;
-							  js = d.createElement(s); js.id = id;
-							  js.src = "//connect.facebook.net/sk_SK/sdk.js#xfbml=1&version=v2.9";
-							  fjs.parentNode.insertBefore(js, fjs);
-							}(document, 'script', 'facebook-jssdk'));
-							</script>
-						<div class="fb-page" data-href="https://www.facebook.com/UAMTFEISTU/?fref=ts" data-tabs="timeline" data-height="430" data-width="1000000000" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false">
-							<blockquote cite="https://www.facebook.com/UAMTFEISTU/?fref=ts" class="fb-xfbml-parse-ignore">
-								<a href="https://www.facebook.com/UAMTFEISTU/?fref=ts">Ústav automobilovej mechatroniky FEI STU</a>
-							</blockquote>
-						</div>
-				   </div>
+				  <div class="col-sm-12 col-md-12">
+					  <div class="hp-thumbnail__wrapper hp-thumbnail__wrapper--small">
+						  <a class="hp-thumbnail__item" style="background-image: url({{ URL::asset('images/indexPhoto/t1.JPG') }})" href="management">
+						  	<div class="hp-thumbnail__item-content">
+						  		<h3>@lang('home::home.about_us')</h3>
+						  		<p class="hp-thumbnail__text">@lang('home::home.about_us_section_preview')</p>
+						  		<p class="hp-thumbnail__link">Prejdite na stránku <i class="fa fa-arrow-right" aria-hidden="true"></i></p>
+						  	</div>
+						  </a>
+					  </div>
 				</div>
-				<!-- Container (Services Section) -->
-<!-- 					<div class="container-fluid text-center hp-benefits">
-				  <h2 class="bold">@lang('home::home.offering')</h2>
-				  <br>
-				  <div class="row">                        
-					<div class="col-sm-4">
-					  <span class="fa fa-graduation-cap hp-benefits__icon" style="color: #4da6ff;"></span>
-					  <h4>@lang('home::home.dis_master_study')</h4>
-					  <p class="con-text">@lang('home::home.dis_master_study_text')</p>
-					</div>
-					<div class="col-sm-4">
-					  <span class="fa fa-certificate hp-benefits__icon" style="color: #4da6ff;"></span>
-					  <h4>@lang('home::home.quality_of_studies')</h4>
-					  <p class="con-text">@lang('home::home.quality_of_studies_text')</p>
-					</div>
-					<div class="col-sm-4">
-					  <span class="fa fa-cog hp-benefits__icon" style="color: #4da6ff;"></span>
-					  <h4>@lang('home::home.interesting_projects')</h4>
-					  <p class="con-text">@lang('home::home.interesting_projects_text')</p>
-					</div>
-				  </div>
-				</div>
-				<div class="row video-index">  
-					<div class="col-md-3"> 
-					</div>
-					<div class="col-md-6 col-xs-12">                    
-						<iframe width="100%" height="320px" src="https://www.youtube.com/embed/vCYq4JspSCI" frameborder="0" allowfullscreen></iframe>
-					</div>
-					<div class="col-md-3"> 
-					</div>
-				</div> -->
 			</div>
 		</div>
-	</div>
-	<section class="hp-events">
-		<div class="container">
-			<div class="row"> 
-				<div class="col-md-7 col-sm-8">
-					<h4>Ústavný kalendár akcií</h4>
-					<div id="datepicker" class="hp-events__datepicker">
-					</div>
+		<div class="col-sm-12 col-md-3">                          
+			<div id="fb-root"></div>
+			<script>(function(d, s, id) {
+			  var js, fjs = d.getElementsByTagName(s)[0];
+			  if (d.getElementById(id)) return;
+			  js = d.createElement(s); js.id = id;
+			  js.src = "//connect.facebook.net/sk_SK/sdk.js#xfbml=1&version=v2.9";
+			  fjs.parentNode.insertBefore(js, fjs);
+			}(document, 'script', 'facebook-jssdk'));
+			</script>
+			<div class="fb-page" data-href="https://www.facebook.com/UAMTFEISTU/?fref=ts" data-tabs="timeline" data-height="600" data-width="1000000000" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false">
+				<blockquote cite="https://www.facebook.com/UAMTFEISTU/?fref=ts" class="fb-xfbml-parse-ignore">
+					<a href="https://www.facebook.com/UAMTFEISTU/?fref=ts">Ústav automobilovej mechatroniky FEI STU</a>
+				</blockquote>
+			</div>						
+		</div>
+		<div class="col-sm-4 col-md-3">
+		  	<div class="hp-events">
+			  	<h4>Ústavný kalendár akcií</h4>
+				<div id="datepicker" class="hp-events__datepicker">
 				</div>
-				<div class=" col-md-offset-1 col-sm-4">
-					<div class="hp-events__list">
-						@php	
-							$maxEvents = 0;
-						@endphp
-						@foreach($events as $key => $event)
-							@if ((getMonth(date("d.m.Y")) <= getMonth(format_time($key))) && (getYear(date("d.m.Y")) <= getYear(format_time($key))))
-								@foreach($event as $e)
-									<h4>{{  $e->name_sk }} @if ($e->text_sk)<i data-toggle="tooltip" data-placement="top" title="{{  $e->text_sk }}" class="fa fa-info-circle"></i> @endif	</h4> 
-									<p>{{  format_time($key) }} 
-										@if ($e->time), {{  $e->time }} @endif 
-										@if ($e->place), {{  $e->place }} @endif
-									</p>	
-									@php
-										if ($maxEvents > 2) {
-											break;
-										}
-										$maxEvents++;
-									@endphp	
-								@endforeach
-							@endif												
-						@endforeach
-					</div>
+			  	<div class="hp-events__list">
+					@php	
+						$maxEvents = 0;
+					@endphp
+					@foreach($events as $key => $event)
+						@if ((getMonth(date("d.m.Y")) <= getMonth(format_time($key))) && (getYear(date("d.m.Y")) <= getYear(format_time($key))))
+							@foreach($event as $e)
+								<h4>{{  $e->name_sk }} @if ($e->text_sk)<i data-toggle="tooltip" data-placement="top" title="{{  $e->text_sk }}" class="fa fa-info-circle"></i> @endif	</h4> 
+								<p>{{  format_time($key) }} 
+									@if ($e->time), {{  $e->time }} @endif 
+									@if ($e->place), {{  $e->place }} @endif
+								</p>	
+								@php
+									if ($maxEvents > 2) {
+										break;
+									}
+									$maxEvents++;
+								@endphp	
+							@endforeach
+						@endif												
+					@endforeach
 				</div>
 			</div>
-		</div>			  
-	</section>
+		</div>  
+	</div>
+	<!-- Container (Services Section) -->
+<!-- <div class="container-fluid text-center hp-benefits">
+	  <h2 class="bold">@lang('home::home.offering')</h2>
+	  <br>
+	  <div class="row">                        
+		<div class="col-sm-4">
+		  <span class="fa fa-graduation-cap hp-benefits__icon" style="color: #4da6ff;"></span>
+		  <h4>@lang('home::home.dis_master_study')</h4>
+		  <p class="con-text">@lang('home::home.dis_master_study_text')</p>
+		</div>
+		<div class="col-sm-4">
+		  <span class="fa fa-certificate hp-benefits__icon" style="color: #4da6ff;"></span>
+		  <h4>@lang('home::home.quality_of_studies')</h4>
+		  <p class="con-text">@lang('home::home.quality_of_studies_text')</p>
+		</div>
+		<div class="col-sm-4">
+		  <span class="fa fa-cog hp-benefits__icon" style="color: #4da6ff;"></span>
+		  <h4>@lang('home::home.interesting_projects')</h4>
+		  <p class="con-text">@lang('home::home.interesting_projects_text')</p>
+		</div>
+	  </div>
+	</div>
+	<div class="row video-index">  
+		<div class="col-md-3"> 
+		</div>
+		<div class="col-md-6 col-xs-12">                    
+			<iframe width="100%" height="320px" src="https://www.youtube.com/embed/vCYq4JspSCI" frameborder="0" allowfullscreen></iframe>
+		</div>
+		<div class="col-md-3"> 
+		</div>
+	</div> -->
 </div>
 
 <script type="text/javascript">
@@ -215,10 +208,10 @@
 
 			$.datepicker.setDefaults($.datepicker.regional['sk']);
 		    $( "#datepicker" ).datepicker({
-				numberOfMonths: 2,
+				numberOfMonths: 1,
 				dateFormat: 'mm.dd.yy',
 				beforeShowDay: function (date) {
-					var m = date.getMonth(), 
+					var m = date.getMonth() + 1, 
 						d = date.getDate(), 
 						y = date.getFullYear();
 					var a = d+"."+m+"."+y; 	
