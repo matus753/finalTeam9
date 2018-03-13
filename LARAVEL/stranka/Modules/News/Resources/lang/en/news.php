fceb656e6cc3d9c73248ede2d49dd877ba55b39d
biac <biacko.ivan@gmail.com>
2018-03-13 03:45:06 +0100

    [IB] preklad public aktualit

---
+++ LARAVEL/stranka/Modules/News/Resources/lang/en/news.php
@@ -0,0 +1,18 @@
+<?php
+return [
+
+    'title' => 'NEWS',
+    'newsletter' => 'Newsletter',
+    'prop' => 'Propagation',
+    'notice' => 'Notice',
+    'life' => 'Institute life',
+    'all' => 'All',
+    'expired' => 'Expired',
+    'more' => 'Read more...',
+    'no_news' => 'No news',
+    'enter_mail' => 'Enter your email address to subscribe to our newsletter',
+    'invalid_mail' => 'Invalid email address',
+    'subscribe' => 'Subscribe',
+    'unsubscribe' => 'Unsubscribe',
+    'type' => 'Type'
+];
---
+++ LARAVEL/stranka/Modules/News/Resources/lang/sk/news.php
@@ -0,0 +1,18 @@
+<?php
+return [
+
+    'title' => 'AKTUALITY',
+    'newsletter' => 'Odber noviniek',
+    'prop' => 'Propagácia',
+    'notice' => 'Oznamy',
+    'life' => 'Zo života ústavu',
+    'all' => 'Všetky',
+    'expired' => 'Expirácia',
+    'more' => 'Zobraziť viac...',
+    'no_news' => 'Žiadne aktuality',
+    'enter_mail' => 'Zadajte svoju emailovú adresu a prihláste sa na odoberanie noviniek',
+    'invalid_mail' => 'Nesprávna emailová adresa',
+    'subscribe' => 'Prihlásiť',
+    'unsubscribe' => 'Zrušiť',
+    'type' => 'Typ'
+];
