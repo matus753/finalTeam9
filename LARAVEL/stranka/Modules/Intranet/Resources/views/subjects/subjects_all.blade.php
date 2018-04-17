b7e5b7d3afca464b4b65941be7b523331e0772f3
biac <biacko.ivan@gmail.com>
2018-04-17 07:10:56 +0200

    [IB] drobny fix predmety

--- LARAVEL/stranka/Modules/Intranet/Resources/views/subjects/subjects_all.blade.php
+++ LARAVEL/stranka/Modules/Intranet/Resources/views/subjects/subjects_all.blade.php
@@ -17,8 +17,8 @@
                 <a href="{{ url('/intranet') }}" class="btn btn-primary"> Späť </a>
             </div>
             <h2>Administácia predmetov</h2>
-                <div class="table-responsive">
-                    <table class="table table-bordered">
+                <div class="table-responsive tableIntra">
+                    <table class="table table-bordered table-stripped intranet-table">
                         <thead>
                         <tr>
                             <th class="col-md-2">Skratka</th>
@@ -33,8 +33,8 @@
                                 <th class="col-md-2">{{ $subject->abbrev }}</th>
                                 <th class="col-md-9"><a href="{{ url('/subjects-admin-info-item/'.$subject->sub_id) }}" >{{ $subject->title }}</a></th>
                                 <th class="col-md-1 text-center">
-                                    <a href ="{{ url('/subjects-admin-edit-item/'.$subject->sub_id) }}" class="btn btn-success btn-xs" ><span class="fa fa-pencil" ></span></a>
-                                    <a href ="{{ url('/subjects-admin-add-item/'.$subject->sub_id) }}" class="btn btn-primary btn-xs" ><span class="fa fa-plus" ></span></a>
+                                    <a href ="{{ url('/subjects-admin-edit-item/'.$subject->sub_id) }}" class="btn btn-success btn-sm" ><span class="fa fa-pencil" ></span></a>
+                                    <a href ="{{ url('/subjects-admin-add-item/'.$subject->sub_id) }}" class="btn btn-primary btn-sm" ><span class="fa fa-plus" ></span></a>
                                 </th>
                             </tr>
                             @foreach($subject->subcategories as $s)
@@ -42,8 +42,8 @@
                                     <td class="col-md-2" style="padding-left:20px;">{{ $subject->abbrev }}</td>
                                     <td class="col-md-9"><a href="{{ url('/subjects-admin-subcategory-show/'.$s->ss_id) }}">{{ $s->name_sk }}</a></td>
                                     <td class="col-md-1 text-center">
-                                        <a href="{{ url('/subjects-admin-edit-item/'.$s->ss_id) }}" class="btn btn-success btn-xs" ><span class="fa fa-pencil" ></span></a>
-                                        <a href="{{ url('/subjects-admin-delete-item/'.$s->ss_id) }}" class="btn btn-danger btn-xs" ><span class="fa fa-trash-o" ></span></a>
+                                        <a href="{{ url('/subjects-admin-edit-item/'.$s->ss_id) }}" class="btn btn-success btn-sm" ><span class="fa fa-pencil" ></span></a>
+                                        <a href="{{ url('/subjects-admin-delete-item/'.$s->ss_id) }}" class="btn btn-danger btn-sm" ><span class="fa fa-trash-o" ></span></a>
                                     </td>
                                 </tr>
                             @endforeach
