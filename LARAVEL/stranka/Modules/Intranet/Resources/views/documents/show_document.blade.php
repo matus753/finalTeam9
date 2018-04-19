09194f93cfd5599cc68b9cb451904bfa704dce25
biac <biacko.ivan@gmail.com>
2018-04-19 19:38:21 +0200

    [IB] view konkretneho dokumentu - zabudol som predtym, sry

--- LARAVEL/stranka/Modules/Intranet/Resources/views/documents/show_document.blade.php
+++ LARAVEL/stranka/Modules/Intranet/Resources/views/documents/show_document.blade.php
@@ -11,26 +11,30 @@
 
 @section('content_admin')
 <div id="emPAGEcontent" class="container">
-    <div class="row">
-        <div class="text-center">
-            <h1>Dokument {{ $document->name_sk }}</h1>
+    <div class="intra-div">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="pull-left">
+                    <a href="{{ url('/documents-admin') }}" class="btn btn-primary btn-back"> Späť </a>
+                </div>
+                <h2>Dokument {{ $document->name_sk }}</h2>
+            </div>
         </div>
+        <hr class="col-md-10 col-md-offset-1">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                {!! $document->text_sk !!}<br>
+            </div>
+        </div>
+        <hr class="col-md-10 col-md-offset-1">
+        <h2>Subory</h2>
+        @foreach($files as $f)
+        <div class="row">
+            <div class="col-md-12">
+                <a href="{{ get_documents_file($category_hash->hash_name, $document->hash_name, $f->file_hash) }}" target="_blank" >{{ $f->file_name }}</a>
+            </div>
+        </div>
+        @endforeach
     </div>
-    <hr>
-	<div class="row">
-		<div class="col-md-12">
-            {!! $document->text_sk !!}<br>
-		</div>
-	</div>
-    <hr>
-    <h2>Subory</h2>
-    @foreach($files as $f)
-	<div class="row">
-		<div class="col-md-12">
-            <a href="{{ get_documents_file($category_hash->hash_name, $document->hash_name, $f->file_hash) }}" target="_blank" >{{ $f->file_name }}</a>
-		</div>
-	</div>
-    @endforeach
 </div>
-
 @stop
