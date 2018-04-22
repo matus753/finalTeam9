0a9237f7ad4ccf64468ef52358a5aa33d3753ef1
Emilia Brisova <milka.brisova@gmail.com>
2018-04-22 14:59:08 +0200

    o nas/vedenie > ked nie je zobrazi -

--- LARAVEL/stranka/Modules/About/Resources/views/management.blade.php
+++ LARAVEL/stranka/Modules/About/Resources/views/management.blade.php
@@ -10,28 +10,67 @@
 </section>
 <div id="emPAGEcontent">
     <div class="container">
+
         <div class="sectContent row">
             <div id="about_div_1" class="col-md-6">
-                <p class="sectText2 bold">@lang('about::aboutUs.head')</p>
+                <p class="bold">@lang('about::aboutUs.head')</p>
+            </div>
+            <div id="about_div_2" class="col-md-6">
+                @if(count($staff1all) == 0)
+                    <p>-</p>
+                @else
+                    @foreach ($staff1all as $staff1)
+                        <p class="sectText2">@if(strcmp($staff1->title1, '' > 1)){{$staff1->title1}}@endif {{$staff1->name}} {{$staff1->surname}}@if(strcmp($staff1->title2, '' > 1)), @endif{{$staff1->title2}}</p>
+                    @endforeach
+                @endif
+            </div>
+        </div>
+
+        <div class="sectContent row">
+            <div id="about_div_1" class="col-md-6">
                 <p class="sectText2 bold">@lang('about::aboutUs.deputy1')</p>
+            </div>
+            <div id="about_div_2" class="col-md-6">
+                @if(count($staff2all) == 0)
+                    <p>-</p>
+                @else
+                    @foreach ($staff2all as $staff2)
+                        <p class="sectText2">@if(strcmp($staff2->title1, '' > 1)){{$staff2->title1}}@endif {{$staff2->name}} {{$staff2->surname}}@if(strcmp($staff2->title2, '' > 1)), @endif{{$staff2->title2}}</p>
+                    @endforeach
+                @endif
+            </div>
+        </div>
+
+        <div class="sectContent row">
+            <div id="about_div_1" class="col-md-6">
                 <p class="sectText2 bold">@lang('about::aboutUs.deputy2')</p>
+            </div>
+            <div id="about_div_2" class="col-md-6">
+                @if(count($staff3all) == 0)
+                    <p>-</p>
+                @else
+                    @foreach ($staff3all as $staff3)
+                        <p class="sectText2">@if(strcmp($staff3->title1, '' > 1)){{$staff3->title1}}@endif {{$staff3->name}} {{$staff3->surname}}@if(strcmp($staff3->title2, '' > 1)), @endif{{$staff3->title2}}</p>
+                    @endforeach
+                @endif
+            </div>
+        </div>
+
+        <div class="sectContent row">
+            <div id="about_div_1" class="col-md-6">
                 <p class="sectText2 bold">@lang('about::aboutUs.deputy3')</p>
             </div>
             <div id="about_div_2" class="col-md-6">
-                @foreach ($staff1all as $staff1)
-                    <p class="sectText2">@if(strcmp($staff1->title1, '' > 1)){{$staff1->title1}}@endif {{$staff1->name}} {{$staff1->surname}}@if(strcmp($staff1->title2, '' > 1)), @endif{{$staff1->title2}}</p>
-                @endforeach
-                    @foreach ($staff2all as $staff2)
-                        <p class="sectText2">@if(strcmp($staff2->title1, '' > 1)){{$staff2->title1}}@endif {{$staff2->name}} {{$staff2->surname}}@if(strcmp($staff2->title2, '' > 1)), @endif{{$staff2->title2}}</p>
-                @endforeach
-                    @foreach ($staff3all as $staff3)
-                        <p class="sectText2">@if(strcmp($staff3->title1, '' > 1)){{$staff3->title1}}@endif {{$staff3->name}} {{$staff3->surname}}@if(strcmp($staff3->title2, '' > 1)), @endif{{$staff3->title2}}</p>
-                @endforeach
+                @if(count($staff4all) == 0)
+                    <p>-</p>
+                @else
                     @foreach ($staff4all as $staff4)
                         <p class="sectText2">@if(strcmp($staff4->title1, '' > 1)){{$staff4->title1}}@endif {{$staff4->name}} {{$staff4->surname}}@if(strcmp($staff4->title2, '' > 1)), @endif{{$staff4->title2}}</p>
-                @endforeach
+                    @endforeach
+                @endif
             </div>
         </div>
+
     </div>
 </div>
 
